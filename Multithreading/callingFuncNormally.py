d3f7684f32800cd0df1117b3a6b454a5d48aab07
Ketan Gupta <56977429+ketangupta1@users.noreply.github.com>
2025-05-23 19:47:46 +0530

    Update and rename callingFuncWithoutMultithreading.py to callingFuncNormally.py

--- Multithreading/callingFuncNormally.py
+++ Multithreading/callingFuncNormally.py
@@ -16,3 +16,9 @@ test.func(1)
 time2 = time.perf_counter()
 
 print(f"Time taken to run all 3 functions: {time2-time1}")
+
+# Output:
+    # Sleeping for 4 seconds.
+    # Sleeping for 2 seconds.
+    # Sleeping for 1 seconds.
+    # Time taken to run all 3 functions: 7.006099754999923
